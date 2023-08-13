da075d411f655441604494055bd6d07bd2059682
Walidoux <ma.walidkorchi@icloud.com>
2023-08-13 23:15:42 +0100

    feat(wasm): implement swc to JSX

--- vite.config.tauri.ts
+++ vite.config.tauri.ts
@@ -3,32 +3,24 @@ import { tauri } from 'vite-plugin-tauri'
 
 import baseViteConfig from './vite.config'
 
-// https://vitejs.dev/config/
 export default defineConfig(
   mergeConfig(
     baseViteConfig,
     defineConfig({
       plugins: [tauri()],
+
       // prevent vite from obscuring rust errors
-
       clearScreen: false,
-      // Tauri expects a fixed port, fail if that port is not available
 
-      server: {
-        strictPort: true,
-        open: false
-      },
+      server: { strictPort: true, open: false },
 
-      // to make use of `TAURI_PLATFORM`, `TAURI_ARCH`, `TAURI_FAMILY`,
+      // env variables: to make use of `TAURI_PLATFORM`, `TAURI_ARCH`, `TAURI_FAMILY`,
       // `TAURI_PLATFORM_VERSION`, `TAURI_PLATFORM_TYPE` and `TAURI_DEBUG`
-      // env variables
       envPrefix: ['VITE_', 'TAURI_'],
+
       build: {
-        // Tauri supports es2021
         target: ['es2022', 'chrome100', 'safari13'],
-        // don't minify for debug builds
         minify: !Boolean(process.env.TAURI_DEBUG) ? 'esbuild' : false,
-        // produce sourcemaps for debug builds
         sourcemap: Boolean(process.env.TAURI_DEBUG)
       }
     })
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,54 @@
+import type { PluginOption } from 'vite'
 import { defineConfig } from 'vite'
 import solidPlugin from 'vite-plugin-solid'
 import devtools from 'solid-devtools/vite'
+import { transformSync } from '@swc/core'
 
-// https://vitejs.dev/config/
+const getExtension = (filename: string): string => {
+  const index = filename.lastIndexOf('.')
+  return index < 0 ? '' : filename.substring(index).replace(/\?.+$/, '')
+}
+
+const SolidSWC: PluginOption = {
+  name: 'solid',
+  transform(code, id) {
+    const currentFileExtension = getExtension(id)
+    const extensionsToWatch = ['.tsx', '.jsx']
+
+    if (!extensionsToWatch.includes(currentFileExtension)) return null
+
+    return transformSync(code, {
+      filename: id,
+      jsc: {
+        parser: { syntax: 'typescript', tsx: true },
+        target: 'es2022',
+        experimental: {
+          plugins: [
+            [
+              new URL('JSX.wasm', import.meta.url).pathname,
+              {
+                module_name: 'solid-js/web'
+              }
+            ]
+          ]
+        }
+      }
+    })
+  }
+}
+
 export default defineConfig({
-  plugins: [devtools({
-    autoname: true,
-    /* locator: {
-      targetIDE: 'vscode',
-      key: 'Ctrl',
-      jsxLocation: true,
-      componentLocation: true,
-    }, */
-  }),solidPlugin()],
+  plugins: [
+    SolidSWC,
+    solidPlugin(),
+    devtools({
+      autoname: true,
+      locator: process.env.NODE_ENV === 'development' && {
+        targetIDE: 'vscode',
+        key: 'Ctrl',
+        jsxLocation: true,
+        componentLocation: true
+      }
+    })
+  ]
 })
