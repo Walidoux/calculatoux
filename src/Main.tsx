357622e20dbb5d1f8210302783c9062d28fb806e
Walidoux <ma.walidkorchi@icloud.com>
2023-08-13 23:15:09 +0100

    refactor(front): complex but solid

--- src/Main.tsx
+++ src/Main.tsx
@@ -5,116 +5,83 @@ import { AiOutlinePercentage } from 'solid-icons/ai'
 import { FaSolidDivide, FaSolidXmark, FaSolidPlus, FaSolidMinus, FaSolidEquals } from 'solid-icons/fa'
 import { TbBackspace } from 'solid-icons/tb'
 import clsx from 'clsx'
-import { Calc } from 'calc-js'
+import { Motion } from '@motionone/solid'
 
-export interface IOPeration {
+import { calculate } from './bindings'
+import { Animations } from './utils/Animations'
+
+export interface IOperation {
   content: JSX.Element
-  ctx: <T extends number>(value: T) => void
-}
-
-export interface Calculator {
-  handler: Calc | null
-  finished: boolean
+  ctx?: Token | (() => unknown)
 }
 
 const App: Component = () => {
-  const [input, setInput] = createSignal<Calculator>({ handler: null, finished: false })
-  const [output, setOuput] = createSignal<number>()
+  const [result, setResult] = createSignal<number>()
+  const [input, setInput] = createSignal<JSX.Element[]>([])
 
-  const [tempNumb, setTempNumb] = createSignal('')
+  const tokens: Token[] = []
 
-  const [error, setError] = createSignal('')
-
-  const updateInput = (newCtx: Calculator): Calculator => setInput((prevCtx) => ({ ...prevCtx, ...newCtx }))
-
-  // After 10 seconds, the operation is considered over
-  const timeOutBeforeFinish = (): NodeJS.Timeout =>
-    setTimeout(() => updateInput({ handler: null, finished: true }), 1000)
-
-  const operations: IOPeration[] = [
-    {
-      content: 'AC',
-      ctx: () => {
-        setOuput()
-        updateInput({ handler: null, finished: true })
-        setTempNumb('')
-      }
-    },
-    { content: <BsPlusSlashMinus />, ctx: () => {} },
-    { content: <AiOutlinePercentage />, ctx: () => {} },
-    { content: <FaSolidDivide />, ctx() {} },
-    { content: 7, ctx: () => {} },
-    { content: 8, ctx: () => {} },
-    { content: 9, ctx: () => {} },
-    { content: <FaSolidXmark />, ctx: (value) => input().handler?.multiply(value) },
-    { content: 4, ctx: () => {} },
-    { content: 5, ctx: () => {} },
-    { content: 6, ctx: () => {} },
-    { content: <FaSolidMinus />, ctx: (value) => input().handler?.minus(value) },
-    { content: 1, ctx: () => {} },
-    { content: 2, ctx: () => {} },
-    { content: 3, ctx: () => {} },
-    { content: <FaSolidPlus />, ctx: (value) => input().handler?.sum(value) },
-    { content: ',', ctx: () => {} },
-    { content: 0, ctx: () => {} },
-    { content: <TbBackspace />, ctx: () => {} },
-    {
-      content: <FaSolidEquals />,
-      ctx: () => {
-        const result = input().handler?.finish()
-
-        console.log(timeOutBeforeFinish())
-
-        setOuput(result)
-      }
-    }
+  const operations: IOperation[] = [
+    { content: 'AC', ctx: () => setInput([]) },
+    { content: <BsPlusSlashMinus />, ctx: '-' },
+    { content: <AiOutlinePercentage />, ctx: '%' },
+    { content: <FaSolidDivide />, ctx: '/' },
+    { content: 7 },
+    { content: 8 },
+    { content: 9 },
+    { content: <FaSolidXmark />, ctx: '*' },
+    { content: 4 },
+    { content: 5 },
+    { content: 6 },
+    { content: <FaSolidMinus />, ctx: '-' },
+    { content: 1 },
+    { content: 2 },
+    { content: 3 },
+    { content: <FaSolidPlus />, ctx: '+' },
+    { content: ',' },
+    { content: 0 },
+    { content: <TbBackspace />, ctx: () => setInput(input().slice(0, -1)) },
+    { content: <FaSolidEquals />, ctx: async () => await calculate(tokens.join('')) }
   ]
 
-  const handleInput = (operation: (typeof operations)[number]): void => {
-    setError('')
-    clearTimeout(timeOutBeforeFinish())
+  const handleTyping = async (operation: IOperation) => {
+    if (typeof operation.ctx === 'function') return operation.ctx()
 
-    if (input().handler == null) {
-      if (operation.content === 'AC') return operation.ctx(0)
-      if (typeof operation.content !== 'number') {
-        setError('Cannot use arethmic operations before starting with a number')
-        return
-      }
+    setInput((prevInput) => {
+      tokens.push(typeof operation.content === 'object' ? operation.ctx : operation.content)
+      return [...prevInput, operation.content]
+    })
 
-      setInput((prevCtx) => ({ ...prevCtx, handler: new Calc(operation.content as number) }))
-    }
-
-    if (typeof operation.content === 'number') {
-      setTempNumb((prevValue) => prevValue.concat(String(operation.content)))
-    } else {
-      operation.ctx(Number(tempNumb()))
-      setTempNumb('')
-    }
+    console.log(input())
   }
 
   return (
     <main class='h-screen overflow-hidden fill-white text-white'>
-      <div class='flex h-1/3 bg-neutral-800/70'>
-        {output()}
-        {tempNumb()}
-        {error()}
+      <div class='flex h-1/3 flex-col bg-gradient-to-tr from-zinc-800/80 to-zinc-700/80 px-3 py-2'>
+        <span class='h-full w-full cursor-default select-none text-right text-3xl hover:opacity-70'>20</span>
+        <ul class='flex h-full w-full items-center justify-end border border-white'>
+          <For each={input()}>
+            {(token) => <Motion.li {...Animations.fadeInOut({ width: [0, '12px', 0] })}>{token}</Motion.li>}
+          </For>
+        </ul>
       </div>
+
       <div class='grid h-2/3 grid-cols-4 grid-rows-5'>
         <For each={operations}>
-          {(ope, index) => {
-            const pointer = Number(index()) + 1
+          {(operation, index) => {
+            const pointer = index() + 1
             const subModule = pointer % 4 !== 0
 
             return (
               <button
-                onClick={() => handleInput(ope)}
-                class={clsx('grid h-full w-full place-items-center text-lg', {
-                  'fill-white/50 text-opacity-50': pointer <= 3,
+                onClick={async () => await handleTyping(operation)}
+                class={clsx('group grid h-full w-full place-items-center text-lg hover:brightness-125', {
                   'bg-zinc-800': subModule,
                   'bg-indigo-500': !subModule,
-                  '!bg-orange-500': index() === operations.length - 1
+                  'bg-orange-500': index() === operations.length - 1,
+                  '!bg-red-500': index() === 0
                 })}>
-                {ope.content}
+                <span class='transition-opacity duration-[25ms] group-active:opacity-30'>{operation.content}</span>
               </button>
             )
           }}
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+// @refresh reload
 import './index.css'
 import 'solid-devtools'
 
@@ -5,6 +6,4 @@ import { render } from 'solid-js/web'
 
 import Main from './Main'
 
-render(() => {
-  return <Main />
-}, document.getElementById('root') as HTMLElement)
+render(() => <Main />, document.getElementById('root') as HTMLElement)
